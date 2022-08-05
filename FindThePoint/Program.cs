ec8ff664ac4eaf942ae5749025018185f5017a82
Tania Akther <taniaakther2113@gmail.com>
2022-08-05 12:15:03 +0600

    Add project files.

---
+++ FindThePoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindThePoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = findPoint(1, 1, 2, 2);
+            Console.WriteLine(result.Aggregate<int, string>(" ", (a, b) => a + " " + b));
+        }
+        public static List<int> findPoint(int px, int py, int qx, int qy)
+        {
+            List<int> list = new List<int>();
+            var rx = qx + (qx - px);
+            list.Add(rx);
+            var ry = qy + (qy - py);
+            list.Add(ry);
+            return list;
+        }
+    }
+}
